2ec3633efc3a8d56a40731c91c58c58fc9d8091e
vovaleontiev <vovaleontiev@ya.ru>
2024-02-19 19:52:13 +0400

    Add TnConfirm

---
+++ src/components/uikit/confirm/tn-confirm-header.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    title: string;
+    subtitle?: string;
+  }>(),
+  {
+    subtitle: "",
+  }
+);
+
+defineEmits(["click"]);
+
+onMounted(() => {
+  console.log("tn-confirm-header: mounted");
+});
+</script>
+
+<template>
+  <div class="tn-confirm-header">
+    <i>tn-confirm-header</i>
+  </div>
+</template>
+
+<style lang="scss">
+.tn-confirm-header {
+  /**/
+}
+</style>
---
+++ src/components/uikit/confirm/tn-confirm.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import TnDialog from "../dialog/tn-dialog.vue";
+import TnButton from "../button/tn-button.vue";
+
+const props = withDefaults(
+  defineProps<{
+    item: any;
+  }>(),
+  {
+    item: null,
+  }
+);
+
+let _resolve: any = () => {};
+let _reject: any = () => {};
+
+function open() {
+  console.log("tn-confirm: open");
+  isOpen.value = true;
+
+  return new Promise((resolve, reject) => {
+    console.log("Create promise", resolve, reject);
+    _resolve = resolve;
+    _reject = reject;
+  }).finally(() => {
+    isOpen.value = false;
+  });
+}
+
+defineExpose({
+  open,
+});
+
+const isOpen = ref(false);
+
+function onReject() {
+  _resolve(false);
+  close();
+}
+function onResolve() {
+  _resolve(true);
+  close();
+}
+
+function close() {
+  _resolve(false);
+  isOpen.value = false;
+}
+
+onMounted(() => {
+  console.log("tn-confirm: mounted", props.item);
+});
+</script>
+
+<template>
+  <TnDialog v-if="isOpen" @close="close" class="tn-confirm">
+    <template #header>
+      <slot name="header">
+        <div class="tn-confirm__header">
+          <h4>Вы уверены?</h4>
+        </div>
+      </slot>
+    </template>
+
+    <slot></slot>
+
+    <template #footer>
+      <div class="tn-confirm__buttons">
+        <slot name="buttons" :reject="onReject" :resolve="onResolve">
+          <TnButton secondary v-focus @click="onReject">Отмена</TnButton>
+          <TnButton primary @click="onResolve">Ок</TnButton>
+        </slot>
+      </div>
+    </template>
+  </TnDialog>
+</template>
+
+<style lang="scss">
+.tn-confirm {
+  /**/
+}
+
+.tn-confirm__buttons {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 16px;
+}
+</style>
